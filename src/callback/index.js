77221304353d82ed3af810659e8be1030c4da271
Jhoss <kuro.rc10@gmail.com>
2020-03-11 08:29:35 -0500

    Realizando segundo ejemplo sobre callbacks

--- src/callback/index.js
+++ src/callback/index.js
@@ -9,3 +9,21 @@ function calc(num1, num2, callback) {
 console.log('Inicio');
 console.log(calc(6, 2, sum));
 console.log('Fin');
+
+// Ejemplo 2
+
+function date(callback) {
+    console.log(new Date);
+    setTimeout(() => {
+        let date = new Date();
+        callback(date);
+    }, 3000);
+}
+
+function printDate(dateNow) {
+    console.log(dateNow);
+}
+
+console.log('Inicio');
+date(printDate);
+console.log('Fin');
