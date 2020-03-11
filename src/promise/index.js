99ef517a855bcdcd467ba7ea22c2533dfc7bc3c4
Jhoss <kuro.rc10@gmail.com>
2020-03-11 11:40:59 -0500

    Usando el Promise.all para verificar si todas las promesas se ejecutaron

--- src/promise/index.js
+++ src/promise/index.js
@@ -11,3 +11,37 @@ const somethingWillHappen = () => {
 somethingWillHappen()
     .then(resp => console.log(resp))
     .catch(err => console.error(err))
+
+// Ejemplo 2
+
+const somethingWillHappen2 = () => {
+    return new Promise((resolve, reject) => {
+        if (true) {
+            setTimeout(() => {
+                resolve('True');
+            }, 2000);
+        } else {
+            const error = new Error('Oops')
+            reject(error);
+        }
+    })
+}
+
+somethingWillHappen2()
+    .then(resp => console.log(resp))
+    .catch(err => console.log(err))
+
+
+/*  
+    Promise.all([]) 
+    Resuelve todas las promesas, es decir, 
+    hasta que no se resuelvan todas no se mostraran en pantalla
+
+*/
+Promise.all([somethingWillHappen(), somethingWillHappen2()])
+    .then(response => {
+        console.log('Array of results', response);
+    })
+    .catch(err => {
+        console.error(err);
+    })
